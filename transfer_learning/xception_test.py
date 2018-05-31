7309be36130cb1557cc9bd22472aef905117771f
ak-lucas <akayamalucas1@gmail.com>
2018-05-31 12:24:25 -0400

    update

--- transfer_learning/xception_test.py
+++ transfer_learning/xception_test.py
@@ -96,42 +96,13 @@ model.compile(loss='categorical_crossentropy',
 							optimizer=opt,
 							metrics=['accuracy'])
 
-print model.summary()
-
-checkpoint = ModelCheckpoint('saved_models/model_{epoch:002d}--{loss:.2f}--{val_loss:.2f}.hdf5',
-              save_best_only=True,
-              save_weights_only=False)
-
-
-# treina e valida o modelo - sem data augmentation
-#model.fit(X_train[train_idx], to_categorical(Y_train[train_idx]),
-#					batch_size=128,
-#					shuffle=True,
-#					epochs=250,
-#					validation_data=(X_train[val_idx], to_categorical(Y_train[val_idx])),
-#					callbacks=[EarlyStopping(min_delta=0.001, patience=10), CSVLogger('training_fold_' + str(fold) + '.log', separator=',', append=False), checkpoint])
-
-# treina e valida o modelo - com data augmentation
-train_generator = datagen_aug.flow_from_directory(path+train_dir, target_size=(299,299),
-																									batch_size=32,
-																									color_mode='rgb',
-																									class_mode='categorical',
-																									seed=7,
-																									)
-val_generator = datagen_no_aug.flow_from_directory(path+val_dir, target_size=(299,299),
+test_generator = datagen_no_aug.flow_from_directory(path+val_dir, target_size=(299,299),
 																									batch_size=32,
 																									color_mode='rgb',
 																									class_mode='categorical',
 																									seed=7)
 
-model.fit_generator(
-									train_generator,workers=1,
-									class_weight={0:3, 1:1}, # balance
-									steps_per_epoch=131, # partition size / batch size
-									epochs=500,
-									shuffle=True,
-                  max_queue_size=30,
-									validation_data=val_generator,
-									callbacks=[EarlyStopping(min_delta=0.001, patience=20), CSVLogger('training.log', separator=',', append=False), checkpoint])
+model.evaluate_generator(
+									test_generator)
 
 #print model.summary()
